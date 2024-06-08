999a4983053f78ad1c7cecbb0b28627add03f541
themanfurious <sioncommunity21@gmail.com>
2024-06-07 22:57:56 -0400

    Initial commit

--- src/main/java/club/flame/disqualified/Disqualified.java
+++ src/main/java/club/flame/disqualified/Disqualified.java
@@ -70,7 +70,7 @@ public final class Disqualified extends JavaPlugin {
 
         if (!this.getDescription().getName().equals("Disqualified") || !this.getDescription().getWebsite().equals("dsc.gg/flameclubdevelopment")
                 || !this.getDescription().getDescription().equals("All in one Network Core")
-                || !this.getDescription().getAuthors().contains("HCFAlerts") || !this.getDescription().getAuthors().contains("FlameClubDevelopment")) {
+                || !this.getDescription().getAuthors().contains("themanfurious") || !this.getDescription().getAuthors().contains("FlameClubDevelopment")) {
             Bukkit.getServer().getPluginManager().disablePlugin(this);
             for (int i = 0; i < 100; i++) {
                 Bukkit.getServer().getConsoleSender().sendMessage("&cWhy are you changing the plugin.yml?");
