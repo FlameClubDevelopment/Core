e6bedafa180553d5ae814ca3ff25a7106d492d2f
themanfurious <sioncommunity21@gmail.com>
2024-06-15 01:03:28 -0400

    fix some bugs

--- src/main/java/club/flame/disqualified/command/essentials/staff/FreezeCommand.java
+++ src/main/java/club/flame/disqualified/command/essentials/staff/FreezeCommand.java
@@ -10,16 +10,22 @@ import club.frozed.lib.commands.CommandArgs;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
 /**
  * Re-Work Code by HCFAlerts
  * Project: Disqualified
  * Credits: FCD
  */
 
-public class FreezeCommand extends BaseCommand {
+public class FreezeCommand extends BaseCommand implements Listener {
 
     @Command(name = "freeze", aliases = {"ss", "congelar"}, permission = "core.essentials.report")
-
     @Override
     public void onCommand(CommandArgs commandArgs) {
         Player player = commandArgs.getPlayer();
@@ -34,7 +40,7 @@ public class FreezeCommand extends BaseCommand {
             return;
         }
         if (target == player) {
-            player.sendMessage(CC.translate("&cError&7! &cYou can't freeze your self."));
+            player.sendMessage(CC.translate("&cError&7! &cYou can't freeze yourself."));
             return;
         }
         PlayerData targetData = PlayerData.getPlayerData(target.getUniqueId());
@@ -42,6 +48,41 @@ public class FreezeCommand extends BaseCommand {
             new PlayerUnFreezeEvent(target, player).call();
         } else {
             new PlayerFreezeEvent(target, player).call();
+        }
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event) {
+        // Listener implementation
+    }
+
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+        PlayerData playerData = PlayerData.getPlayerData(player.getUniqueId());
+        if (playerData.isFreeze()) {
+            String message = event.getMessage().toLowerCase();
+            if (!message.startsWith("/msg") && !message.startsWith("/message") && !message.startsWith("/r")) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        PlayerData playerData = PlayerData.getPlayerData(player.getUniqueId());
+        if (playerData.isFreeze()) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onDmg(EntityDamageEvent event) {
+        Player player = (Player) event.getEntity();
+        PlayerData playerData = PlayerData.getPlayerData(player.getUniqueId());
+        if (playerData.isFreeze()) {
+            event.setCancelled(true);
         }
     }
 }
