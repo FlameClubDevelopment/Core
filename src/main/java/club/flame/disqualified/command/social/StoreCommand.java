4ee4d508af93351cd29e7ebab7b5b1b9c91f2955
themanfurious <sioncommunity21@gmail.com>
2024-06-15 00:55:44 -0400

    fix some bugs

--- src/main/java/club/flame/disqualified/command/social/StoreCommand.java
+++ src/main/java/club/flame/disqualified/command/social/StoreCommand.java
@@ -8,17 +8,33 @@ import club.frozed.lib.commands.CommandArgs;
 import club.flame.disqualified.utils.lang.Lang;
 import org.bukkit.entity.Player;
 
-public class StoreCommand extends BaseCommand {
-    @Command(name = "store")
+public class DiscordCommand extends BaseCommand {
 
+    private static final String COMMAND_NAME = "discord";
+    private static final String MESSAGE_KEY = "COMMANDS.SOCIAL.MESSAGES";
+
+    @Command(name = COMMAND_NAME)
     @Override
     public void onCommand(CommandArgs cmd) {
-        Player p = cmd.getPlayer();
+        Player player = cmd.getPlayer();
+
         if (cmd.getArgs().length == 0) {
-            p.sendMessage(CC.translate(Disqualified.getInstance().getMessagesConfig().getConfiguration().getString("COMMANDS.SOCIAL.MESSAGES")
-                    .replace("<command>", "Store")
-                    .replace("<social>", Lang.STORE))
-            );
+            String message = getFormattedMessage();
+            if (message != null) {
+                player.sendMessage(CC.translate(message));
+            } else {
+                player.sendMessage(CC.translate("&cMessage not found in configuration."));
+            }
+        } else {
+            player.sendMessage(CC.translate("&cUsage: /" + COMMAND_NAME));
         }
+    }
+
+    private String getFormattedMessage() {
+        String messageTemplate = Disqualified.getInstance().getMessagesConfig().getConfiguration().getString(MESSAGE_KEY);
+        if (messageTemplate == null) {
+            return null;
+        }
+        return messageTemplate.replace("<command>", "Discord").replace("<social>", Lang.DISCORD);
     }
 }
