ef62eea52fb497e30d30914bbc6c4260644fe2b3
themanfurious <sioncommunity21@gmail.com>
2024-06-15 11:07:53 -0400

    fix some bugs

--- src/main/java/club/flame/disqualified/command/social/TeamSpeakCommand.java
+++ src/main/java/club/flame/disqualified/command/social/TeamSpeakCommand.java
@@ -9,16 +9,22 @@ import club.flame.disqualified.utils.lang.Lang;
 import org.bukkit.entity.Player;
 
 public class TeamSpeakCommand extends BaseCommand {
+    
     @Command(name = "teamspeak", aliases = {"ts", "ts3"})
-
     @Override
     public void onCommand(CommandArgs cmd) {
-        Player p = cmd.getPlayer();
+        Player player = cmd.getPlayer();
+        
         if (cmd.getArgs().length == 0) {
-            p.sendMessage(CC.translate(Disqualified.getInstance().getMessagesConfig().getConfiguration().getString("COMMANDS.SOCIAL.MESSAGES")
-                    .replace("<command>", "TeamSpeak")
-                    .replace("<social>", Lang.TS))
-            );
+            String message = Disqualified.getInstance()
+                                        .getMessagesConfig()
+                                        .getConfiguration()
+                                        .getString("COMMANDS.SOCIAL.MESSAGES");
+            if (message != null) {
+                message = message.replace("<command>", "TeamSpeak")
+                                 .replace("<social>", Lang.TS);
+                player.sendMessage(CC.translate(message));
+            }
         }
     }
 }
