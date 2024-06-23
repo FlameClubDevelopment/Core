dcd9107b294f94a0e48f49e53f25014725e0eaa5
FlameClubDevelopment <flameclubdevelopment@outlook.com>
2024-06-23 15:44:04 -0300

    Fixed

--- src/main/java/club/flame/disqualified/command/chat/ChatManagerCommand.java
+++ src/main/java/club/flame/disqualified/command/chat/ChatManagerCommand.java
@@ -2,11 +2,13 @@ package club.flame.disqualified.command.chat;
 
 import club.flame.disqualified.Disqualified;
 import club.flame.disqualified.manager.chat.ChatManager;
+import club.flame.disqualified.utils.Utils;
 import club.frozed.lib.chat.CC;
 import club.frozed.lib.commands.BaseCommand;
 import club.frozed.lib.commands.Command;
 import club.frozed.lib.commands.CommandArgs;
 import club.frozed.lib.commands.Completer;
+import club.frozed.lib.config.ConfigCursor;
 import club.frozed.lib.number.NumberUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -16,36 +18,78 @@ import java.util.List;
 
 public class ChatManagerCommand extends BaseCommand {
 
-    @Command(name = "chat.slow", permission = "chat.slow")
-    public void onChatSlow(CommandArgs args) {
-        Player player = args.getPlayer();
-        String[] arguments = args.getArgs();
+    @Completer(name = "chat", aliases = {"chatmanager"})
+    public List<String> ChatManagerComplete(CommandArgs args) {
+        List<String> list = new ArrayList<>();
+        list.add("clear");
+        list.add("mute");
+        list.add("unmute");
+        list.add("delay");
 
-        if (arguments.length < 1) {
-            player.sendMessage(CC.RED + "Usage: /chat slow <seconds>");
-            return;
-        }
-
-        if (!NumberUtils.isInteger(arguments[0])) {
-            player.sendMessage(CC.RED + "The delay must be a number.");
-            return;
-        }
-
-        int delay = Integer.parseInt(arguments[0]);
-        ChatManager chatManager = Disqualified.getInstance().getChatManager();
-        chatManager.setChatDelay(delay);
-
-        Bukkit.broadcastMessage(CC.translate("&eChat has been slowed by &c" + delay + " &eseconds."));
+        return list;
     }
 
-    @Completer(name = "chat.slow")
-    public List<String> onChatSlowComplete(CommandArgs args) {
-        List<String> completions = new ArrayList<>();
-        completions.add("5");
-        completions.add("10");
-        completions.add("15");
-        completions.add("30");
-        completions.add("60");
-        return completions;
+    @Command(name = "chat", permission = "core.manager", aliases = {"chatmanager"}, inGameOnly = true)
+    @Override
+    public void onCommand(CommandArgs cmd) {
+        Player player = cmd.getPlayer();
+        String[] args = cmd.getArgs();
+        ConfigCursor messages = new ConfigCursor(Disqualified.getInstance().getMessagesConfig(), "NETWORK.CHAT-MANAGER");
+        ChatManager chatManager = Disqualified.getInstance().getChatManager();
+
+        if (args.length == 0) {
+            player.sendMessage(CC.CHAT_BAR);
+            player.sendMessage(CC.translate("&4&lDisqualified&7 &4✖ &8- &7Chat Management"));
+            player.sendMessage(CC.translate(" "));
+            player.sendMessage(CC.translate("&4 ▸ &f/chat clear"));
+            player.sendMessage(CC.translate(" "));
+            player.sendMessage(CC.translate("&4 ▸ &f/chat mute"));
+            player.sendMessage(CC.translate("&4 ▸ &f/chat unmute"));
+            player.sendMessage(CC.translate(" "));
+            player.sendMessage(CC.translate("&4 ▸ &f/chat delay <time>"));
+            player.sendMessage(CC.CHAT_BAR);
+            return;
+        }
+
+        switch (args[0]) {
+            case "clear":
+                for (int i = 0; i < 120; i++) {
+                    Utils.sendAllMsg("");
+                }
+                Utils.sendAllMsg(CC.translate(messages.getString("CLEAR").replace("<player>", player.getDisplayName())));
+                break;
+            case "mute":
+                if (!chatManager.isMute()) {
+                    chatManager.setMute(true);
+                    Bukkit.broadcastMessage(CC.translate(messages.getString("MUTE").replace("<player>", player.getName())));
+                } else {
+                    player.sendMessage(CC.translate(messages.getString("ALREADY").replace("<label>", "muted")));
+                }
+                chatManager.save();
+                break;
+            case "unmute":
+                if (chatManager.isMute()) {
+                    chatManager.setMute(false);
+                    Bukkit.broadcastMessage(CC.translate(messages.getString("UNMUTE").replace("<player>", player.getName())));
+                } else {
+                    player.sendMessage(CC.translate(messages.getString("ALREADY").replace("<label>", "unmuted")));
+                }
+                chatManager.save();
+                break;
+            case "delay":
+                if (!NumberUtils.checkInt(args[1])) {
+                    player.sendMessage("§cIt must be a number");
+                    return;
+                }
+                chatManager.setDelay(Integer.parseInt(args[1]));
+                Bukkit.broadcastMessage(CC.translate(messages.getString("DELAY")
+                        .replace("<delay>", args[1])
+                        .replace("<player>", player.getName()))
+                );
+                chatManager.save();
+                break;
+            default:
+                break;
+        }
     }
 }
